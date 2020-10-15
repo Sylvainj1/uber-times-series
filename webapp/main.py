6cf951caaf1a53a02b91d275fc9e3565893e2800
jeremys <jeremysurget77@gmail.com>
2020-10-15 22:00:24 +0200

    1h forecast Manhattan working (still WIP)

--- webapp/main.py
+++ webapp/main.py
@@ -12,7 +12,7 @@ app.config.from_envvar('APP_CONFIG_FILE', silent=True)
 MAPBOX_ACCESS_KEY = app.config['MAPBOX_ACCESS_KEY']
 
 
-def create_pickups_cluster():
+def create_pickups_cluster(prediction_number=5300):
     schema = {
         "type": "FeatureCollection",
         "crs": {"type": "name", "properties": {"name": "pickups_json"}},
@@ -21,7 +21,7 @@ def create_pickups_cluster():
 
     populate_feature = []
 
-    for i in range(0, 5300):
+    for i in range(0, prediction_number):
         feature = {"type": "Feature", "properties": {"id": "Manhattan", "time": 1507425650893},
                    "geometry": {"type": "Point", "coordinates": [-73.968565, 40.779897, 0.0]}}
         populate_feature.append(feature)
@@ -48,11 +48,20 @@ def predict():
     # genre 1h, 2h etc...
     # recuperer cet input ici 
     #et choper la valeur en conséquence
-    # par exemple ici au lieu d'avoir prediction[0] pour 1h apres,
-    # #si l'utilisateur veut 2h apres ca sera prediction[1] etc...
-    
+    # par exemple ici au lieu d'avoir predictions[0] pour 1h apres,
+    # #si l'utilisateur veut 2h apres ca sera predictions[1] etc...
+
     features = pd.read_csv('../X_test_csv.csv')
     predictions = model_test.predict(features)
+
+    cluster_value = int(predictions[0])
+    json_to_pass = create_pickups_cluster(cluster_value)
+
+    return render_template(
+        'mapbox_js.html',
+        ACCESS_KEY=MAPBOX_ACCESS_KEY,
+        pickup_data=json.dumps(json_to_pass)
+    )
 
 
 if __name__ == '__main__':
